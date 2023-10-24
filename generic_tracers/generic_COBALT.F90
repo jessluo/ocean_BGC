3dee1b26110b5a9fb678a257fde5cff3b8125f81
Jessica Luo <jessluo@gmail.com>
2023-10-24 16:15:11 -0400

    v. 6.15 Sept 15 2021

--- generic_tracers/generic_COBALT.F90
+++ generic_tracers/generic_COBALT.F90
@@ -5788,7 +5788,7 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('imax_smz',zoo(1)%imax, 1.42 / sperd)              ! s-1
     call g_tracer_add_param('imax_mdz',zoo(2)%imax, 0.57 / sperd)              ! s-1
     call g_tracer_add_param('imax_lgz',zoo(3)%imax, 0.23 / sperd)              ! s-1
-    call g_tracer_add_param('imax_smt',zoo(4)%imax, 2.5 / sperd)              ! s-1 ! note that imax and ki !
+    call g_tracer_add_param('imax_smt',zoo(4)%imax, 0.75*2.5 / sperd)              ! s-1 ! note that imax and ki !
     call g_tracer_add_param('imax_lgt',zoo(5)%imax, 0.8 / sperd)               ! s-1 ! are scaled together   !
     call g_tracer_add_param('ki_smz',zoo(1)%ki, 1.25e-6)                       ! moles N kg-1
     call g_tracer_add_param('ki_mdz',zoo(2)%ki, 1.25e-6)                       ! moles N kg-1
@@ -5892,7 +5892,7 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('bresp_mdz',zoo(2)%bresp, 0.008 / sperd)             ! s-1
     call g_tracer_add_param('bresp_lgz',zoo(3)%bresp, 0.0032 / sperd)            ! s-1
     call g_tracer_add_param('bresp_smt',zoo(4)%bresp, 0.047 / sperd)             ! s-1
-    call g_tracer_add_param('bresp_lgt',zoo(5)%bresp, 0.75*0.011 / sperd)         ! s-1
+    call g_tracer_add_param('bresp_lgt',zoo(5)%bresp, 0.035 / sperd)         ! s-1
 
     call g_tracer_add_param('phi_aresp_smz',zoo(1)%phi_aresp, 0.3)               ! dimensionless
     call g_tracer_add_param('phi_aresp_mdz',zoo(2)%phi_aresp, 0.3)               ! dimensionless
@@ -5914,7 +5914,7 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('kae_smz',zoo(1)%kae, 1.0e+10)                     ! moles N kg-1
     call g_tracer_add_param('kae_mdz',zoo(2)%kae, 1.0e+10)                     ! moles N kg-1
     call g_tracer_add_param('kae_lgz',zoo(3)%kae, 1.0e+10)                     ! moles N kg-1
-    call g_tracer_add_param('kae_smt',zoo(4)%kae, 1.41e-6)                     ! moles N kg-1
+    call g_tracer_add_param('kae_smt',zoo(4)%kae, 1.34e-6)                     ! moles N kg-1
     call g_tracer_add_param('kae_lgt',zoo(5)%kae, 2.63e-6)                     ! moles N kg-1
 
     call g_tracer_add_param('agg_smz',zoo(1)%agg, 0.0)                         ! s-1 (moles N kg)-1
