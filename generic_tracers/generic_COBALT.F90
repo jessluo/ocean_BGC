9941656ae399fcf646bcf39a92ad62028fe7df47
Jessica Luo <jessluo@gmail.com>
2023-10-24 16:18:46 -0400

    v. 6.22 Jan 29 2022

--- generic_tracers/generic_COBALT.F90
+++ generic_tracers/generic_COBALT.F90
@@ -5793,7 +5793,7 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('imax_mdz',zoo(2)%imax, 0.57 / sperd)              ! s-1
     call g_tracer_add_param('imax_lgz',zoo(3)%imax, 0.23 / sperd)              ! s-1
     call g_tracer_add_param('imax_smt',zoo(4)%imax, 0.75*2.5 / sperd)              ! s-1 ! note that imax and ki !
-    call g_tracer_add_param('imax_lgt',zoo(5)%imax, 0.6 / sperd)               ! s-1 ! are scaled together   !
+    call g_tracer_add_param('imax_lgt',zoo(5)%imax, 0.55 / sperd)               ! s-1 ! are scaled together   !
     call g_tracer_add_param('ki_smz',zoo(1)%ki, 1.25e-6)                       ! moles N kg-1
     call g_tracer_add_param('ki_mdz',zoo(2)%ki, 1.25e-6)                       ! moles N kg-1
     call g_tracer_add_param('ki_lgz',zoo(3)%ki, 1.25e-6)                       ! moles N kg-1
@@ -5896,7 +5896,7 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('bresp_mdz',zoo(2)%bresp, 0.008 / sperd)             ! s-1
     call g_tracer_add_param('bresp_lgz',zoo(3)%bresp, 0.0032 / sperd)            ! s-1
     call g_tracer_add_param('bresp_smt',zoo(4)%bresp, 0.06 / sperd)              ! s-1
-    call g_tracer_add_param('bresp_lgt',zoo(5)%bresp, 0.01 / sperd)              ! s-1
+    call g_tracer_add_param('bresp_lgt',zoo(5)%bresp, 0.009 / sperd)              ! s-1
 
     call g_tracer_add_param('phi_aresp_smz',zoo(1)%phi_aresp, 0.3)               ! dimensionless
     call g_tracer_add_param('phi_aresp_mdz',zoo(2)%phi_aresp, 0.3)               ! dimensionless
