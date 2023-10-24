1cd04d0a77993df0510569c93777c3181ebb1446
Jessica Luo <jessluo@gmail.com>
2023-10-24 16:05:50 -0400

    v. 6.4 Apr 04 2021

--- generic_tracers/generic_COBALT.F90
+++ generic_tracers/generic_COBALT.F90
@@ -5898,7 +5898,7 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('bresp_mdz',zoo(2)%bresp, 0.008 / sperd)             ! s-1
     call g_tracer_add_param('bresp_lgz',zoo(3)%bresp, 0.0032 / sperd)            ! s-1
     call g_tracer_add_param('bresp_smt',zoo(4)%bresp, 0.059 / sperd)             ! s-1
-    call g_tracer_add_param('bresp_lgt',zoo(5)%bresp, 0.76*0.035 / sperd)        ! s-1
+    call g_tracer_add_param('bresp_lgt',zoo(5)%bresp, 0.1*0.035 / sperd)        ! s-1
 
     call g_tracer_add_param('phi_aresp_smz',zoo(1)%phi_aresp, 0.3)               ! dimensionless
     call g_tracer_add_param('phi_aresp_mdz',zoo(2)%phi_aresp, 0.3)               ! dimensionless
