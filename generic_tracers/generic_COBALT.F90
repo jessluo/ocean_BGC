a99509457c5168e7dba76c52b718eacfa024b14f
Jessica Luo <jessluo@gmail.com>
2023-10-25 14:02:56 -0400

    v. 6.23d Jan 30 2022 (final version, sensitivity expt 4)

--- generic_tracers/generic_COBALT.F90
+++ generic_tracers/generic_COBALT.F90
@@ -5792,13 +5792,13 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('imax_smz',zoo(1)%imax, 1.42 / sperd)              ! s-1
     call g_tracer_add_param('imax_mdz',zoo(2)%imax, 0.57 / sperd)              ! s-1
     call g_tracer_add_param('imax_lgz',zoo(3)%imax, 0.23 / sperd)              ! s-1
-    call g_tracer_add_param('imax_smt',zoo(4)%imax, 0.765 / sperd)              ! s-1 ! note that imax and ki !
-    call g_tracer_add_param('imax_lgt',zoo(5)%imax, 0.22 / sperd)               ! s-1 ! are scaled together   !
+    call g_tracer_add_param('imax_smt',zoo(4)%imax, 0.75*2.5 / sperd)              ! s-1 ! note that imax and ki !
+    call g_tracer_add_param('imax_lgt',zoo(5)%imax, 0.55 / sperd)               ! s-1 ! are scaled together   !
     call g_tracer_add_param('ki_smz',zoo(1)%ki, 1.25e-6)                       ! moles N kg-1
     call g_tracer_add_param('ki_mdz',zoo(2)%ki, 1.25e-6)                       ! moles N kg-1
     call g_tracer_add_param('ki_lgz',zoo(3)%ki, 1.25e-6)                       ! moles N kg-1
-    call g_tracer_add_param('ki_smt',zoo(4)%ki, 1.25e-6)                       ! moles N kg-1 ! note that imax and ki !
-    call g_tracer_add_param('ki_lgt',zoo(5)%ki, 1.25e-6)                       ! moles N kg-1 ! are scaled together   !
+    call g_tracer_add_param('ki_smt',zoo(4)%ki, 3.05e-6)                       ! moles N kg-1 ! note that imax and ki !
+    call g_tracer_add_param('ki_lgt',zoo(5)%ki, 3.05e-6)                       ! moles N kg-1 ! are scaled together   !
     call g_tracer_add_param('ktemp_smz',zoo(1)%ktemp, 0.063)                   ! C-1
     call g_tracer_add_param('ktemp_mdz',zoo(2)%ktemp, 0.063)                   ! C-1
     call g_tracer_add_param('ktemp_lgz',zoo(3)%ktemp, 0.063)                   ! C-1
@@ -5907,19 +5907,19 @@ write (stdlogunit, generic_COBALT_nml)
     call g_tracer_add_param('assim_eff_max_smz',zoo(1)%assim_eff_max, 0.7)       ! dimensionless
     call g_tracer_add_param('assim_eff_max_mdz',zoo(2)%assim_eff_max, 0.7)       ! dimensionless
     call g_tracer_add_param('assim_eff_max_lgz',zoo(3)%assim_eff_max, 0.7)       ! dimensionless
-    call g_tracer_add_param('assim_eff_max_smt',zoo(4)%assim_eff_max, 0.8)       ! dimensionless
-    call g_tracer_add_param('assim_eff_max_lgt',zoo(5)%assim_eff_max, 0.8)       ! dimensionless
+    call g_tracer_add_param('assim_eff_max_smt',zoo(4)%assim_eff_max, 0.6)       ! dimensionless
+    call g_tracer_add_param('assim_eff_max_lgt',zoo(5)%assim_eff_max, 0.6)       ! dimensionless
     call g_tracer_add_param('assim_eff_min_smz',zoo(1)%assim_eff_min, 0.7)       ! dimensionless
     call g_tracer_add_param('assim_eff_min_mdz',zoo(2)%assim_eff_min, 0.7)       ! dimensionless
     call g_tracer_add_param('assim_eff_min_lgz',zoo(3)%assim_eff_min, 0.7)       ! dimensionless
-    call g_tracer_add_param('assim_eff_min_smt',zoo(4)%assim_eff_min, 0.25)      ! dimensionless
-    call g_tracer_add_param('assim_eff_min_lgt',zoo(5)%assim_eff_min, 0.25)      ! dimensionless
+    call g_tracer_add_param('assim_eff_min_smt',zoo(4)%assim_eff_min, 0.6)      ! dimensionless
+    call g_tracer_add_param('assim_eff_min_lgt',zoo(5)%assim_eff_min, 0.6)      ! dimensionless
 
     call g_tracer_add_param('kae_smz',zoo(1)%kae, 1.0e+10)                     ! moles N kg-1
     call g_tracer_add_param('kae_mdz',zoo(2)%kae, 1.0e+10)                     ! moles N kg-1
     call g_tracer_add_param('kae_lgz',zoo(3)%kae, 1.0e+10)                     ! moles N kg-1
-    call g_tracer_add_param('kae_smt',zoo(4)%kae, 1.34e-6)                     ! moles N kg-1
-    call g_tracer_add_param('kae_lgt',zoo(5)%kae, 2.63e-6)                     ! moles N kg-1
+    call g_tracer_add_param('kae_smt',zoo(4)%kae, 1.0e+10)                     ! moles N kg-1
+    call g_tracer_add_param('kae_lgt',zoo(5)%kae, 1.0e+10)                     ! moles N kg-1
 
     call g_tracer_add_param('agg_smz',zoo(1)%agg, 0.0)                         ! s-1 (moles N kg)-1
     call g_tracer_add_param('agg_mdz',zoo(2)%agg, 0.0)                         ! s-1 (moles N kg)-1
